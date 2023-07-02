b0acfe8eaa8a2de111d7116bd0fa3e2ca9d52fed
Karolinaa12 <110235732+Karolinaa12@users.noreply.github.com>
2023-07-02 23:50:23 +0200

    Changed into 12 hour clock with sessions

--- app.js
+++ app.js
@@ -5,8 +5,22 @@ function showTime() {
   var h = date.getHours();
   var m = date.getMinutes();
   var s = date.getSeconds();
+  var session = "AM";
 
-  clock.innerHTML = `${h}:${m}:${s}`;
+  if (h == 0) {
+    h = 12;
+  }
+
+  if (h > 12) {
+    h = h - 12;
+    session = "PM";
+  }
+
+  h = h < 10 ? "0" + h : h;
+  m = m < 10 ? "0" + m : m;
+  s = s < 10 ? "0" + s : s;
+
+  clock.innerHTML = `${h}:${m}:${s} ${session}`;
   setTimeout(showTime, 1000);
 }
 
